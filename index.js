c414327859946a1c80c2061c458467bfbdeb38a4
sebastian-meier <contact@sebastianmeier.eu>
2017-07-17 20:11:08 +0200

    multi interpolator

--- index.js
+++ index.js
@@ -17,3 +17,46 @@ SVIFT.helper.debouncer = function ( func , timeout ) {
     } , timeout );
   };
 };
+
+/*
+ * D3 standard interpolate can only handle two values, this interpolate function creates a series of interpolations betweens elements
+ * 
+ * @param {array} `a` [{value:0}, {value:50, duration:10, ease:d3.cubicInOut}, {value:100, duration:5, ease:d3.cubicInOut}]
+ *
+ */
+
+SVIFT.helper.interpolate = function ( a ) {
+
+  var ip = {};
+
+  ip.def = a;
+  ip.interpolate = [];
+  ip.duration = 0;
+
+  for(var i = 1; i<ip.def.length; i++){
+    if(!('ease' in ip.def[i])){ip.def[i].ease = false;}
+    ip.interpolate.push(d3.interpolate(ip.def[i-1].value, ip.def[i].value));
+    ip.duration += ip.def[i].duration;
+  }
+
+  var t_duration = 0;
+  for(var i = 1; i<ip.def.length; i++){
+    ip.def[i].duration = ip.def[i].duration / ip.duration;
+    ip.def[i].start = t_duration;
+    t_duration += ip.def[i].duration;
+  }
+
+  ip.i = function (t) {
+    for(var i = 1; i<ip.def.length; i++){
+      if(t>=ip.def[i].start && t<=(ip.def[i].start+ip.def[i].duration)){
+        if(ip.def[i].ease){
+          return ip.interpolate[i](ip.def[i].ease((t-start)/duration));
+        }else{
+          return ip.interpolate[i]((t-start)/duration);
+        }
+      }
+    }
+  };
+
+  return ip;
+};
